6e1026797e0decd42f7e1eef77f34f563d91308a
Gabriela Santos <gabrielasnts30@gmail.com>
2025-04-07 21:45:24 -0300

    feat(frontend): initial setup of Next.js with main page

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main className="m-auto flex min-h-lvh items-center h-full justify-center w-full p-4">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-8xl text-secondary font-bold text-center cursor-default">
+          Flashcards
+        </h1>
+        <p className="text-2xl font-bold text-center cursor-default">
+          A simple flashcards app.
+        </p>
+        <button className="mt-4 bg-gray-200 border dark:text-black text-white border-primary text-primary py-2 px-4 rounded-xl">
+          <a href="/signup">Get Started</a>
+        </button>
+      </div>
+    </main>
+  );
+}
